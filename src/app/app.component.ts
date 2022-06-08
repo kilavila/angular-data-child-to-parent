1dafe533ea7b3596591c27ef882b154284cde1af
kilavila <christer.kilavik@protonmail.com>
2022-06-08 11:30:59 +0200

    Form example with reusable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  displayedText: string = 'Change me';
+  newsletterForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
 
-  onSubmit(eventData: { text: string }): void {
+  constructor() {}
+
+  ngOnInit(): void {
+    const demoFormRaw = localStorage.getItem('demo_form');
+    console.log(demoFormRaw);
+
+    if (demoFormRaw) {
+      const demoForm = JSON.parse(demoFormRaw);
+
+      this.newsletterForm.patchValue({
+        name: demoForm.name,
+        email: demoForm.email,
+      });
+    }
+  }
+
+  onFormSubmit(): void {
+    console.log(this.newsletterForm.value);
+    localStorage.setItem('demo_form', JSON.stringify(this.newsletterForm.value));
+  }
+
+  onNameChange(eventData: { text: string }): void {
     console.log(eventData);
-    this.displayedText = eventData.text;
+    this.newsletterForm.patchValue({ name: eventData.text });
+  }
+
+  onEmailChange(eventData: { text: string }): void {
+    console.log(eventData);
+    this.newsletterForm.patchValue({ email: eventData.text });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
@@ -10,7 +11,8 @@ import { ChildComponent } from './child/child.component';
     ChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
